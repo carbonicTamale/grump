a518e959c5b83ba410fa4d9cb6c42df54bfebc5a
Matthew Brooks <mdeanbrooks@gmail.com>
2015-10-07 14:46:53 -0700

    Test for pull

--- cmds/use.js
+++ cmds/use.js
@@ -3,4 +3,9 @@ var fs    = require('fs');
 var color = require('colors');
 var utils = require('../utils.js');
 
-module.exports = function (alias) {};
+module.exports = function (alias) {
+  alias = alias ? alias[0] : null;
+  var message = alias ? "Now using the alias " + alias.cyan : "Alias cleared.";
+  console.log(message);
+  return alias;
+};
