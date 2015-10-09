2f89858e165f8793b93d79407fd54dd75f791ab3
Marcus Buffett <marcusbuffett@me.com>
2015-10-09 10:36:51 -0700

    Selecting multiple installed grumps

--- cmds/run.js
+++ cmds/run.js
@@ -6,38 +6,38 @@ var inquirer = require('inquirer');
 
 module.exports = function(args, installedGrumps) {
 
+  if (utils.isVerbose()) {
+    console.log("Received grump collection: " + JSON.stringify(installedGrumps));
+  }
   // Make sure we are receiving a grump to run
   if (args.length === 0) {
     console.log("Error".red + ": Please provide a grump to run.");
     return;
-  }
-
-  if (utils.isVerbose()) {
-    console.log("Received grump collection: " + installedGrumps[args[0]].cyan);
-
-  // Or else just run the grump
-  } else {
+  } 
+  else {
     if (!installedGrumps.hasOwnProperty(args[0])) {
       console.log('Grump not found.');
     }
     else if (installedGrumps[args[0]].length > 1) {
       console.log('Multiple grumps exist');
 
-      var choices = res.grumps.map(function(grump) {
-        return grump.author + "/" + grump.defaultCommand;
+      var choices = installedGrumps[args[0]].map(function(grump) {
+        return grump.author + "/" + grump.command;
       });
 
       var question = {
         type: 'list',
         choices: choices,
-        message: 'Choose a grump to install',
-        name: 'install'
+        message: 'Choose a grump to run',
+        name: 'run'
       };
 
 
       inquirer.prompt([question], function( answers ) {
-        var chosenIndex = choices.indexOf(answers.install);
-        utils.install(res.grumps[chosenIndex], installedGrumps);
+        var chosenIndex = choices.indexOf(answers.run);
+        console.log(chosenIndex);
+        console.log(installedGrumps[args[0]][chosenIndex]);
+        utils.run(installedGrumps[args[0]][chosenIndex], args.slice(1));
       });
 
       
@@ -47,28 +47,5 @@ module.exports = function(args, installedGrumps) {
     }
 
 
-    // // Specific grump.
-    // if (grump.indexOf("/") !== -1) {
-    //   utils.run(grump, args.slice(1));
-
-    // // General
-    // } else {
-    //   // If more than one local grump installed with same name
-    //   if (installedGrumps[2][grump].length > 1) {
-    //     console.log("Found multiple local grumps named " + grump.cyan + ".");
-    //     console.log("Please choose a specific grump from the list below and rerun your command.\n");
-
-    //     installedGrumps[2][grump].forEach(function(grump) {
-    //       console.log("\t" + grump[0].green + "/" + grump[1].cyan);
-    //     });
-
-    //     console.log("\n");
-
-    //   // Since only 1 local grump exists, assume that is the one the user wanted.
-    //   } else {
-    //     var chosen = installedGrumps[2][grump][0];
-    //     utils.run(chosen[0] + "/" + chosen[1], args.slice(1));
-    //   }
-    // }
-  }
-};
+  // Or else just run the grump
+}};
